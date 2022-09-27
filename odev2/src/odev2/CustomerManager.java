bacc4edfb5b30a38d504ba8cef73e7221f9b7ea8
Mustafa Başdemir <basdemir803@gmail.com>
2022-09-27 15:28:40 +0300

    Add files via upload

---
+++ odev2/src/odev2/CustomerManager.java
@@ -0,0 +1,22 @@
+package odev2;
+
+public class CustomerManager {
+
+	public void add()
+	{
+		System.out.println("EKLEME İŞLEMİ TAMAMLANDI.");
+		
+	}
+	
+	public void update()
+	{
+		System.out.println("GÜNCELLEME  İŞLEMİ TAMAMLANDI.");
+	}
+	
+	public void remove()
+	{
+		System.out.println("SİLME İŞLEMİ TAMAMLANDI.");
+	}
+	
+	
+}
